75b120b4ee51d3bf8dc3677568ceb5e2f5484469
spitzerc22 <spitzerc22@gmail.com>
2021-12-08 12:03:51 -0600

    tried it

--- public/index.js
+++ public/index.js
@@ -1,9 +1,26 @@
-async function main() {
+const { GME, MSFT, DIS, BNTX } = mockData;
 
+const stocks = [GME, MSFT, DIS, BNTX];
+
+console.log(stocks)
+
+
+async function renderCharts(chart) {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
 }
+    
+
+async function main() {
+
+    const response = await fetch ("https://api.twelvedata.com/api_usage?apikey=97676cb08d4741809abcb0c86f2095d0");
+        const charts = await response.json();
+       return charts;
+ 
+}
+
+
+
 
 main()
